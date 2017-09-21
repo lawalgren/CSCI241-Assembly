85fb6ec52229f7bebfc929abdddd340180f5e875
Lucas Walgren <lawalgren@mavs.coloradomesa.edu>
2017-09-21 11:19:06 -0600

    Modified the sample.s file

--- source/sample.s
+++ source/sample.s
@@ -1,11 +1,27 @@
 Message:
 	.ascii "Hello World\0"
 	.globl	main
+	
+i: .long 0
 main:
 	push	%rbp
 	mov		%rsp, %rbp
 	sub		$32, %rsp
+	
 	/*  SUB, SBB, ADD, ADC, CLR, STC, CLC */
+	
+	// i=0;
+	//movl $0, %eax
+	xor %eax, %eax
+	movl %eax, i
+	movl $0xA5A5A5A5, %eax
+	movl i, %eax
+	
+	// i++; i = i + 1;
+	movl i, %eax
+	add $1, %eax
+	movl %eax, i
+	
 	movl $01, %eax
 	movw $0xff, %ax
 	movq $0xff, %rax
