97d0e6bb17fc526772ad7a0913b463bee2e4975b
Lucas Walgren <lawalgren@mavs.coloradomesa.edu>
2017-10-02 18:44:46 -0600

    wrote a bindechex that used bit-shifting for binary and hex

--- source/binDecHex.s
+++ source/binDecHex.s
@@ -1,14 +1,10 @@
 .section .data
-	
+
 i:
-	.long  1234
-j:
-	.long  0
-k:
-	.byte  0
+	.long 2297
 
 stack:
-	.rept 32
+	.rept 17
 	.long 0x00
 	.endr
 
@@ -19,43 +15,106 @@ main:
 	push	%rbp
 	mov		%rsp, %rbp
 	sub		$32, %rsp
-	
-	mov 	i(%rip), %rax
-	mov		%rax, j(%rip)
-bin:
-	mov		j(%rip), %rax
-	cmp		$0, %rax
-	je		dbin
-	xor		%rcx, %rcx
-	shr		%rax
-	mov		%rax, j(%rip)
+
+	mov 	i(%rip), %rbx
+	xor		%rsi, %rsi
+binary:
+	cmp		$0, %rbx
+	je		outputbin
+
+	shr		%rbx
+	lea		stack, %rax
 	jc		one
-	mov		$0x30, %rcx
-	call	putchar
-	jmp	bin
+	movl	$0x30, (%rax, %rsi, 4)
+	inc		%rsi
 
+	jmp		binary
 one:
-	mov		$0x31, %rcx
-	call	putchar
-	jmp 	bin
-dbin:
-	mov		i(%rip), %rax
-	and		$1, %rax
-	cmp		$0, %rax
-	je		addz
+	movl	$0x31, (%rax, %rsi, 4)
+	inc		%rsi
 
-nline:
-	mov		$0xa, %rcx
+	jmp		binary
+outputbin:
+	cmp   $0, %rsi
+	je		endbin
+
+	dec		%rsi
+	lea		stack, %rax
+	mov		(%rax, %rsi, 4), %rcx
 	call	putchar
-	jmp		dec
-	
-addz:
-	mov		$0, %rcx
+	lea		stack, %rax
+	movl	$0, (%rax, %rsi, 4)
+
+	jmp		outputbin
+endbin:
+	mov		$0xA, %rcx
 	call	putchar
-	jmp 	nline
-dec:
-	
-	
+	mov		i(%rip), %rax
+	mov		$10, %rbx
+	xor		%rsi, %rsi
+decimal:
+	cmp		$0, %rax
+	je		outputdec
+
+	xor		%rdx, %rdx
+	div		%rbx
+	lea		stack, %rcx
+	mov		%rdx, (%rcx, %rsi, 4)
+	inc		%rsi
+
+	jmp		decimal
+outputdec:
+	cmp		$0, %rsi
+	je		enddec
+
+	dec		%rsi
+	lea		stack, %rax
+	mov		(%rax, %rsi, 4), %rcx
+	add		$0x30, %rcx
+	call	putchar
+	lea		stack, %rax
+	movl	$0, (%rax, %rsi, 4)
+
+	jmp		outputdec
+enddec:
+	mov		$0xA, %rcx
+	call	putchar
+	mov		i, %rax
+	xor		%rsi, %rsi
+hex:
+	cmp		$0, %rax
+	je		outputhex
+
+	mov		%rax, %rbx
+	and		$0xF, %rbx
+	lea		stack, %rcx
+	mov		%rbx, (%rcx, %rsi, 4)
+	inc		%rsi
+	shr		$4, %rax
+
+	jmp		hex
+outputhex:
+	cmp		$0, %rsi
+	je		endhex
+
+	dec		%rsi
+	lea		stack, %rax
+	mov		(%rax, %rsi, 4), %rcx
+
+	and		$0xf, %rcx
+	cmp		$9, %rcx
+	jg		letter
+	add		$0x30, %rcx
+	call	putchar
+
+	jmp		outputhex
+letter:
+	add		$0x37, %rcx
+	call 	putchar
+
+	jmp 	outputhex
+endhex:
+
 	mov		$0, %eax
 	add		$32, %rsp
 	pop		%rbp
