4d93e404e036849578df849acc5f4a076848d443
Lucas Walgren <lawalgren@mavs.coloradomesa.edu>
2017-10-03 12:57:57 -0600

    compressed the multipurpose conversion program

--- source/betterBinDecHex.s
+++ source/betterBinDecHex.s
@@ -1,15 +1,15 @@
 .section .data
 i:
-	.long 15559
-	
+	.long 999595
+
 stack:
 	.rept 32
 	.long 0x00
 	.endr
-	
+
 position:
 	.long 1
-	 
+
 	.text
 	.globl	main
 
@@ -19,46 +19,37 @@ main:
 	sub		$32, %rsp
 
 top:
-	xor		%rsi, %rsi
-	lea		stack(%rip), %rax
-startclear:
-	cmp 	$32, %rsi
-	je		cleared
-	movl	$0, (%rax, %rsi, 4)
-	inc		%rsi
-	jmp	startclear
-cleared:
-	
+
 	mov		position(%rip), %rax
-	
+
 	cmp		$1, %rax
 	je		bin
 
 	cmp 	$2, %rax
-	je		dec
+	je		deci
 
 	cmp		$3,	%rax
 	je		hex
-	
+
 	jmp		done
-	
+
 bin:
 	mov		i(%rip), %rax
 	mov		$2, %rbx
 	xor		%rsi, %rsi
 	jmp		convert
-	
-dec:	
+
+deci:
 	mov		i(%rip), %rax
 	mov		$10,  %rbx
 	xor		%rsi, %rsi
 	jmp		convert
-	
+
 hex:
 	mov		i(%rip), %rax
 	mov 	$16, %rbx
 	xor		%rsi, %rsi
-		
+
 convert:
 	cmp		$0, %rax
 	je		output
@@ -75,11 +66,12 @@ output:
 	dec 	%rsi
 	lea		stack(%rip), %rax
 	mov		(%rax, %rsi, 4), %rcx
-	
+	movl		$0, (%rax, %rsi, 4)
+
 	and		$0xff, %rcx
 	cmp		$9, %rcx
 	ja		letter
-	
+
 	add		$0x30, %rcx
 	call	putchar
 	jmp 	output
@@ -91,9 +83,9 @@ end:
 	mov		$0xa, %rcx
 	call	putchar
 	incl	position(%rip)
-	jmp		top	
+	jmp		top
 done:
-	
+
 	mov		$0, %eax
 	add		$32, %rsp
 	pop		%rbp
