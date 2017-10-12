cbef8261291dbda569bd5fa000cbe1ae8b3e74af
Lucas Walgren <lawalgren@mavs.coloradomesa.edu>
2017-10-11 22:41:17 -0600

    Created functional stringToInt code

---
+++ source/stringToInt.s
@@ -0,0 +1,32 @@
+.include "macros.s"
+programStart
+string nString,"1234"
+int base,10
+int workingNum,0
+
+lea nString, %ebx
+xor %edx,%edx
+calculate:
+mov (%ebx),%cl
+cmp $0x30, %cl
+jb endcalc
+mov workingNum,%eax
+mull base
+mov %eax,workingNum
+inc %ebx
+
+cmp $0x39,%cl
+jg  hex
+
+sub $0x30,%cl
+add %cl,workingNum
+jmp calculate
+
+hex:
+sub $0x37,%cl
+add %cl,workingNum
+jmp calculate
+endcalc:
+outputInt workingNum
+
+programEnd
